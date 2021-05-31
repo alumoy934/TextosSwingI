39cd5cc7ee49b474226e2df9ceed51cbc1a90def
carmoy <alumoy934@ieselcaminas.org>
2021-05-31 12:00:48 +0200

    Textos en java 1

--- src/LaminaTexto.java
+++ src/LaminaTexto.java
@@ -1,25 +1,53 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LaminaTexto extends JPanel {
 
+    private JLabel resultado;
+
     public LaminaTexto(){
+
+        setLayout(new BorderLayout());
+
+        JPanel lamina2 = new JPanel();
+        lamina2.setLayout(new FlowLayout());
+
+        resultado = new JLabel("", JLabel.CENTER);
+
+
         JLabel label = new JLabel("Introduce un texto");
-        add(label);
+        lamina2.add(label);
 
         JTextField campo1 = new JTextField(10);
-        add(campo1);
-
-        System.out.println(campo1.getText());
+        lamina2.add(campo1);
 
         JButton boton = new JButton("Presiona el botón");
-        add(boton);
+        lamina2.add(boton);
+
+        add(lamina2, BorderLayout.NORTH);
+        add(resultado, BorderLayout.CENTER);
 
         boton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Texto introducido: " + campo1.getText());
+
+                String email = campo1.getText().trim();
+                int correcto = 0;
+
+                for (int i = 0; i < email.length(); i++) {
+                    if (email.charAt(i) == '@'){
+                        correcto ++;
+                    }
+                }
+
+                if (correcto !=1){
+                   resultado.setText("Email incorrecto");
+                }else {
+                    resultado.setText("Email correcto");
+                }
+
             }
         });
     }
